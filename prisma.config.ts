6ff44169917b1a2833dca43b4f401a103c3ac585
nikop111997-blip <nikop111997@gmail.com>
2026-07-31 16:49:57 +0530

    bakcend

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,26 +1,12 @@
-import { PrismaClient } from "@prisma/client";
-import { Pool } from "pg";
-import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
+import { defineConfig } from "prisma/config";
 
-// 1. Initialize the raw database connection pool
-const connectionString = `${process.env.DATABASE_URL}`;
-const pool = new Pool({ connectionString });
-
-// 2. Wrap it in the Prisma adapter
-const adapter = new PrismaPg(pool);
-
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-export const db =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    adapter, // 3. Pass the adapter to the PrismaClient constructor here!
-    log:
-      process.env.NODE_ENV === "development"
-        ? ["error", "warn"]
-        : ["error"],
-  });
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = db;
-}
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: process.env.DATABASE_URL || "postgresql://localhost:5432/dummy",
+  },
+});
